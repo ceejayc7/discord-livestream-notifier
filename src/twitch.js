f1abf293cc40509874cc8578a1f445b965491958
Ceejay Chand <ceejayc7@gmail.com>
2018-07-14 18:09:37 -0700

    updates to twitch

--- src/twitch.js
+++ src/twitch.js
@@ -11,27 +11,54 @@ let options = {
     },
     json: true,
     method: "GET"
-};
+},
+    currentLiveStreams = [];
 
 function updateStreams() {
     options.url = constants.TWITCH_API_ENDPOINT+streams.twitch.toString();
+    request(options, requestResponseCallback);
 }
 
-function parseResponse(response) {
-    console.log(_.get(response, 'streams'));
+
+function reduceResponse(response) {
+    let reducedResponse = [];
+    _.forOwn(response.streams, function(stream) {
+        reducedResponse.push(
+            {
+                "platform" : "twitch",
+                "channelName": _.get(stream, ['channel','display_name']),
+                "game": _.get(stream, 'game'),
+                "preview": _.get(stream, ['preview', 'medium']),
+                "viewers": _.get(stream, 'viewers'),
+                "title": _.get(stream, ['channel','status']),
+                "logo": _.get(stream, ['channel','logo']),
+                "url": _.get(stream, ['channel','url'])
+            }
+        );
+    });
+    return reducedResponse;
 }
 
 function logError(error) {
     console.log('Twitch API error: ' + error);
 }
 
-function callback(error, response, body) {
-    if(!error && response.statusCode == 200) {
-        parseResponse(body);
+function announceIfStreamIsNew(stream) {
+    let currentLiveChannels = _.map(currentLiveStreams, 'channelName');
+    if(!_.includes(currentLiveChannels, stream.channelName)) {
+        console.log(stream);
+        return true;
+    }
+}
+
+function requestResponseCallback(error, response, body) {
+    if(!error && response.statusCode === 200) {
+        let newStreams = reduceResponse(body);
+        _.forEach(newStreams, announceIfStreamIsNew); 
+        
     } else {
         logError(error);
     }
 }
 
 updateStreams();
-request(options, callback);
