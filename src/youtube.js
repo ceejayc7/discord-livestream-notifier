a207c88d52f3a3cc4ff28e9d38bae4ab7cf47c48
ceejayc7 <ceejayc7@gmail.com>
2019-08-30 18:55:01 -0700

    add whitelist youtube streams on 24/7 streams cons

--- src/youtube.js
+++ src/youtube.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import request from 'request-promise';
 import { Helpers } from './helpers';
-import { YOUTUBE_KEY } from './constants';
+import { YOUTUBE_KEY, WHITELIST_ALL_YOUTUBE_STREAMS } from './constants';
 
 const PLATFORM = 'youtube';
 const YOUTUBE_API_ENDPOINT = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&eventType=live&key=${YOUTUBE_KEY}&channelId=`;
@@ -35,21 +35,24 @@ class Youtube {
   reduceResponse = (response) => {
     const reducedResponse = [];
     const flattenedResponse = _.flatten(_.map(response, 'items'));
-    _.forOwn(flattenedResponse, function(stream) {
+    _.forOwn(flattenedResponse, (stream) => {
       if (stream) {
-        const url = `https://www.youtube.com/watch?v=${_.get(stream, ['id', 'videoId'])}`;
-        const preview =
-          _.get(stream, ['snippet', 'thumbnails', 'high', 'url']) +
-          `?t=${Math.round(new Date().getTime() / 1000)}`;
-        reducedResponse.push({
-          platform: PLATFORM,
-          name: _.get(stream, ['snippet', 'channelId']),
-          channelTitle: _.get(stream, ['snippet', 'channelTitle']),
-          preview: preview,
-          title: _.get(stream, ['snippet', 'title']),
-          url: url,
-          updated_at: new Date().toISOString()
-        });
+        const title = _.get(stream, ['snippet', 'title']);
+        if (WHITELIST_ALL_YOUTUBE_STREAMS || !_.includes(title, '24/7')) {
+          const url = `https://www.youtube.com/watch?v=${_.get(stream, ['id', 'videoId'])}`;
+          const preview =
+            _.get(stream, ['snippet', 'thumbnails', 'high', 'url']) +
+            `?t=${Math.round(new Date().getTime() / 1000)}`;
+          reducedResponse.push({
+            platform: PLATFORM,
+            name: _.get(stream, ['snippet', 'channelId']),
+            channelTitle: _.get(stream, ['snippet', 'channelTitle']),
+            preview: preview,
+            title,
+            url: url,
+            updated_at: new Date().toISOString()
+          });
+        }
       }
     });
     return reducedResponse;
