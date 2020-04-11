b80c13ca3f515012c93588a9d0821e7abcda624d
ceejayc7 <ceejayc7@gmail.com>
2020-04-11 13:04:30 -0700

    add constants to repo

---
+++ src/constants.js
@@ -0,0 +1,118 @@
+export const BOT_COMMANDS = {
+  SLOTS: {
+    command: '!slots',
+    showOnHelp: true,
+  },
+  SLOTS_LB: {
+    command: '!slotslb',
+    showOnHelp: true,
+  },
+  BLACKJACK: {
+    command: '!21',
+    showOnHelp: true,
+  },
+  BLACKJACK_HIT: {
+    command: '!hit',
+    showOnHelp: false,
+  },
+  BLACKJACK_HIT_SHORTHAND: {
+    command: '!h',
+    showOnHelp: false,
+  },
+  BLACKJACK_STAND: {
+    command: '!stand',
+    showOnHelp: false,
+  },
+  BLACKJACK_STAND_SHORTHAND: {
+    command: '!s',
+    showOnHelp: false,
+  },
+  BLACKJACK_DOUBLE: {
+    command: '!double',
+    showOnHelp: false,
+  },
+  BLACKJACK_DOUBLE_SHORTHAND: {
+    command: '!d',
+    showOnHelp: false,
+  },
+  BLACKJACK_SPLIT: {
+    command: '!split',
+    showOnHelp: false,
+  },
+  BITCOIN: {
+    command: '!bitcoin',
+    showOnHelp: true,
+  },
+  BITCOIN_SHORTHAND: {
+    command: '!btc',
+    showOnHelp: false,
+  },
+  LEADERBOARD: {
+    command: '!leaderboard',
+    showOnHelp: true,
+  },
+  LEADERBOARD_SHORTHAND: {
+    command: '!lb',
+    showOnHelp: false,
+  },
+  HELP: {
+    command: '!help',
+    showOnHelp: false,
+  },
+  FISH: {
+    command: '!fish',
+    showOnHelp: false,
+  },
+  FISH_LEADERBOARD: {
+    command: '!fishlb',
+    showOnHelp: false,
+  },
+  LOTTO: {
+    command: '!lotto',
+    showOnHelp: true,
+  },
+  LOTTO_CLAIM: {
+    command: '!claim',
+    showOnHelp: false,
+  },
+  KPOP: {
+    command: '!kpop',
+    showOnHelp: false,
+  },
+  GENERATE: {
+    command: '!iptv',
+    showOnHelp: false,
+  },
+  TWEET: {
+    command: '!tweet',
+    showOnHelp: false,
+  },
+};
+export const SLOTS_MONEY = {
+  SLOTS_COST: 10,
+  SLOTS_DOUBLE_REWARD: 100,
+  SLOTS_TRIPLE_REWARD: 10000,
+  SLOTS_QUADS_REWARD: 100000,
+  SLOTS_WIN_REWARD: 100000000,
+};
+export const BLACKJACK_MONEY = {
+  BLACKJACK_PAYOUT: 1.5,
+};
+export const LOTTO_MAX = 100000;
+export const MINIMUM_BITCOINS = 10;
+export const PLAYERS = 'players';
+export const LOTTO = 'lotto';
+
+export const IPTV_DATABASE = {
+  'SBS MTV': 'sbsmtv.txt',
+  'SBS F!L UHD': 'sbsfil.txt',
+  'MBC Music': 'mbcmusic.txt',
+  'MBC Every1': 'mbcevery1.txt',
+  Mnet: 'mnet.txt',
+  '아리랑 TV': 'arirang_hd.txt',
+  KBS2: 'kbs2.txt',
+  MBC: 'mbc.txt',
+  SBS: 'sbs.txt',
+  'SBS Fun E': 'sbsfune.txt',
+  'SBS Plus': 'sbsplus.txt',
+};
