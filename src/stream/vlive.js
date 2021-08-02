2214753bc11ac5ff355f45712c89bee98ee08d59
ceejayc7 <ceejayc7@gmail.com>
2021-08-01 23:26:43 -0700

    migrate vlive to new api endpoint

--- src/stream/vlive.js
+++ src/stream/vlive.js
@@ -1,13 +1,9 @@
 import Livestream from '@stream/livestream';
 import _ from 'lodash';
-import moment from 'moment-timezone';
 import request from 'request-promise';
 
-const APP_ID = '8c6cc7b45d2568fb668be6e05b6e5a3b';
-const CHANNEL_ID = '{CHANNEL_ID}';
-const VLIVE_API_ENDPOINT = `https://api-vfan.vlive.tv/v2/channel.${CHANNEL_ID}/home?gcc=US&locale=en&app_id=${APP_ID}&limit=20`;
-const VLIVE_VIDEO = 'https://www.vlive.tv/video/';
-const IMG_REGEX = new RegExp(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/, 'gi');
+const BOARD_ID = '{BOARD_ID}';
+const VLIVE_API_ENDPOINT = `https://www.vlive.tv/globalv-web/vam-web/post/v1.0/board-${BOARD_ID}/posts?appId=8c6cc7b45d2568fb668be6e05b6e5a3b&fields=author,officialVideo,url&sortType=LATEST&limit=20&gcc=US&locale=en_US`;
 
 class Vlive extends Livestream {
   constructor(sendStreamMessageToServers, silentMode) {
@@ -25,18 +21,22 @@ class Vlive extends Livestream {
     this.getAPIDataAndAnnounce(this.useReduceResponse, this.useMultipleCalls).catch();
   };
 
-  getChannelPromises = (channelId) => {
+  getChannelPromises = (boardId) => {
     const httpOptions = {
-      url: VLIVE_API_ENDPOINT.replace(CHANNEL_ID, channelId),
+      url: VLIVE_API_ENDPOINT.replace(BOARD_ID, boardId),
+      headers: {
+        Host: 'vlive.tv'
+      },
       json: true
     };
+
     return request(httpOptions)
       .then((response) => {
         const data = _.first(
-          _.filter(response.contentList, (stream) => stream.videoType === 'LIVE')
+          _.filter(response.data, (stream) => stream?.officialVideo?.type === 'LIVE')
         );
         if (data) {
-          data.id = channelId;
+          data.id = boardId;
         }
         return data;
       })
@@ -46,30 +46,15 @@ class Vlive extends Livestream {
   reduceResponse = (response) => {
     const reducedResponse = [];
     for (const stream of response) {
-      const timestamp = moment
-        .tz(stream?.onAirStartAt, 'YYYY-MM-DD HH:mm:ss', 'Asia/Seoul')
-        .tz('America/Los_Angeles')
-        .toISOString();
-
-      // magic logic to generate a thumbnail
-      let thumbnail = stream?.thumbnail;
-      if (!IMG_REGEX.test(thumbnail)) {
-        const width = '1024';
-        const height = '576';
-        thumbnail = encodeURI(
-          `https://dthumb-phinf.pstatic.net/?src="${thumbnail}?type=ff${width}_${height}"&twidth=${width}&theight=${height}&opts=12`
-        );
-      }
-
       reducedResponse.push({
         platform: this.PLATFORM,
         name: stream?.id,
-        displayName: stream?.representChannelName,
-        logo: stream?.representChannelProfileImg,
-        preview: thumbnail,
-        title: stream?.title,
-        url: VLIVE_VIDEO + stream?.videoSeq,
-        updatedAt: timestamp
+        displayName: stream?.author?.nickname,
+        logo: stream?.author?.profileImageUrl,
+        preview: stream?.officialVideo?.thumb,
+        title: stream?.officialVideo?.title,
+        url: stream?.url,
+        updatedAt: Date(stream?.officialVideo?.onAirStartAt)
       });
     }
     return reducedResponse;
