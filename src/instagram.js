61930c8f3994dbf66273bac12d01721d5afca09a
ceejayc7 <ceejayc7@gmail.com>
2021-11-27 01:15:07 -0800

    add instagram proxy if main api fails

--- src/instagram.js
+++ src/instagram.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+
 import Discord from 'discord.js';
 import _ from 'lodash';
 import { getWebhook } from '@root/webhook';
@@ -5,6 +7,7 @@ import request from 'request-promise';
 import { sendMessageToChannel } from '@root/util';
 
 const INSTAGRAM_REGEX = /instagram\.com\/(p|tv)\/(.*)/;
+const TOKENS = require('@data/constants.json')?.tokens?.instagram;
 
 export const doesMsgContainInstagram = (msg) => INSTAGRAM_REGEX.test(msg.content);
 
@@ -21,44 +24,58 @@ const getInstagramId = (msg) => {
   return '';
 };
 
-const fetchApiData = (id) => {
-  const httpOptions = {
+const fetchApiData = async (id) => {
+  let httpOptions = {
     url: `https://instagram.com/p/${id}/?__a=1`,
     json: true
   };
-  return request(httpOptions);
+  const result = await request(httpOptions);
+
+  // main api throttled, try proxy
+  if (!result?.graphql) {
+    console.log('Instagram: Using RapidAPI');
+    const post = `https://www.instagram.com/p/${id}`;
+    httpOptions = {
+      url: `https://${TOKENS?.HOST}/media-info-by-url?url=${encodeURIComponent(post)}`,
+      headers: {
+        'x-rapidapi-host': `${TOKENS?.HOST}`,
+        'x-rapidapi-key': `${TOKENS?.KEY}`
+      },
+      json: true
+    };
+    return request(httpOptions);
+  }
+  return result?.graphql.shortcode_media;
 };
 
 const parseApiData = (apiData) => {
   const videos = [];
   const pictures = [];
-  const edges = _.get(apiData, 'graphql.shortcode_media.edge_sidecar_to_children.edges', []);
+  const edges = apiData?.edge_sidecar_to_children?.edges ?? [];
   for (const edge of edges) {
     edge.node.is_video ? videos.push(edge.node.video_url) : pictures.push(edge.node.display_url);
   }
 
   // singular video/picture post
   if (_.isEmpty(edges)) {
-    apiData.graphql.shortcode_media.is_video
-      ? videos.push(apiData.graphql.shortcode_media.video_url)
-      : pictures.push(apiData.graphql.shortcode_media.display_url);
+    apiData.is_video ? videos.push(apiData.video_url) : pictures.push(apiData.display_url);
   }
 
-  let text = _.get(apiData, 'graphql.shortcode_media.edge_media_to_caption.edges');
+  let text = apiData?.edge_media_to_caption?.edges ?? [];
   if (!_.isEmpty(text)) {
-    text = _.get(text[0], 'node.text');
+    text = text?.[0]?.node?.text;
   } else {
     text = '';
   }
 
   return {
-    name: apiData.graphql.shortcode_media.owner.full_name,
-    username: apiData.graphql.shortcode_media.owner.username,
+    name: apiData.owner.full_name,
+    username: apiData.owner.username,
     text,
     videos,
     pictures,
-    timestamp: apiData.graphql.shortcode_media.taken_at_timestamp * 1000,
-    avatar: apiData.graphql.shortcode_media.owner.profile_pic_url
+    timestamp: apiData.taken_at_timestamp * 1000,
+    avatar: apiData.owner.profile_pic_url
   };
 };
 
@@ -122,10 +139,4 @@ const sendMediaToChannel = async (msg, media, embeds, webhook) => {
   if (media.videos.length) {
     media.videos.map((videoURL) => sendMessageToChannel(msg, videoURL));
   }
-
-  // sometimes the official discord ig embed doesnt show until later
-  // not sure why
-  // just wait 2 seconds and try to suppress it again
-  // await wait(2000);
-  // msg.suppressEmbeds(true);
 };
