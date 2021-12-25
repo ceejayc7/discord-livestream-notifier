3d00dc0ad2ed3133c3d7ab5ea057c954861896ca
ceejayc7 <ceejayc7@gmail.com>
2021-12-24 19:40:09 -0800

    increase retry count

--- src/request/instagram.js
+++ src/request/instagram.js
@@ -41,7 +41,7 @@ export const getInstagramStories = async (id) => {
 };
 
 const getUserPostDataFromProxy = async (id, retryAttempt) => {
-  if (retryAttempt === 2) {
+  if (retryAttempt === 4) {
     console.log(`Unable to get proxy data for id: ${id} after retryAttempt: ${retryAttempt}`);
     return null;
   }
